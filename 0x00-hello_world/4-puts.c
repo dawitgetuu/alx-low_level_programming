52f73d20df23c9e0f3bf8a6bacfd870632c0e622
dawitgetuu <archfun3@gmail.com>
2023-09-07 05:28:19 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with puts funcitio4-puts.cn
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
